57d6604171b0f361e7adfa19e01d780dfa09b151
CamDawg <pcamagna@yahoo.com>
2022-02-08 05:38:09 -0800

    COP dialogue tree for TotLM events has bad trigger

--- iwdfixpack/dlg/dplanar.d
+++ iwdfixpack/dlg/dplanar.d
@@ -1,17 +1,14 @@
 // totlm dialogue for contact other plane
 APPEND DPLANAR
 
-  IF WEIGHT #-1 ~GlobalGT("Master_Quest","GLOBAL",0) GlobalLT("Master_Quest","GLOBAL",8)~ THEN BEGIN cespy0 SAY @100
+  IF WEIGHT #-1 ~GlobalGT("Master_Quest","GLOBAL",0) GlobalLT("Master_Quest","GLOBAL",7)~ THEN BEGIN cespy0 SAY @100
     IF ~~ THEN REPLY @101 GOTO cespy_where
     IF ~~ THEN REPLY @102 GOTO cespy_hobart
     IF ~~ THEN REPLY @103 GOTO cespy_lure
     IF ~~ THEN REPLY @104 GOTO cespy_return
     IF ~~ THEN REPLY @105 GOTO cespy_malu
     IF ~Global("Know_Vexing","GLOBAL",1)
-        !Global("Know_Truename","GLOBAL",1)
-        OR(2)
-          PartyHasItem("vexed")
-          PartyHasItem("vexed2")~ THEN DO ~SetGlobal("Know_Truename","GLOBAL",1)~ REPLY #25298 GOTO cespy_vex
+        !Global("Know_Truename","GLOBAL",1)~ THEN DO ~SetGlobal("Know_Truename","GLOBAL",1)~ REPLY #25298 GOTO cespy_vex
     IF ~~ THEN REPLY #25129 GOTO cespy_bye
   END
 
